590ebf1cd39308f105ea6f9df8a01e11e0c23ca3
onebone <jyc00410@gmail.com>
2020-11-14 03:33:18 +0900

    refactor(klas-api): all methods should return Single instead of Observable

--- app/src/main/java/org/openklas/klas/KlasClient.kt
+++ app/src/main/java/org/openklas/klas/KlasClient.kt
@@ -2,7 +2,6 @@ package org.openklas.klas
 
 import android.util.Base64
 import com.google.gson.Gson
-import io.reactivex.Observable
 import io.reactivex.Single
 import org.openklas.klas.error.KlasSigninFailError
 import org.openklas.klas.model.Home
@@ -20,42 +19,41 @@ class KlasClient @Inject constructor(
 ) {
 	private var semesters: Array<Semester>? = null
 
-	fun login(username: String, password: String): Observable<String> {
+	fun login(username: String, password: String): Single<String> {
 		val securityResponse = service.loginSecurity()
-		return securityResponse.toObservable()
-			.flatMap { security ->
-				val keyFactory = KeyFactory.getInstance("RSA")
-				val keySpec = X509EncodedKeySpec(Base64.decode(security.publicKey, Base64.DEFAULT))
-				val publicKey = keyFactory.generatePublic(keySpec)
+		return securityResponse.flatMap { security ->
+			val keyFactory = KeyFactory.getInstance("RSA")
+			val keySpec = X509EncodedKeySpec(Base64.decode(security.publicKey, Base64.DEFAULT))
+			val publicKey = keyFactory.generatePublic(keySpec)
 
-				val cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding")
-				cipher.init(Cipher.ENCRYPT_MODE, publicKey)
+			val cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding")
+			cipher.init(Cipher.ENCRYPT_MODE, publicKey)
 
-				val cipherText = cipher.doFinal(gson.toJson(mapOf(
-					"loginId" to username,
-					"loginPwd" to password,
-					"storeIdYn" to "N"
-				)).toByteArray())
+			val cipherText = cipher.doFinal(gson.toJson(mapOf(
+				"loginId" to username,
+				"loginPwd" to password,
+				"storeIdYn" to "N"
+			)).toByteArray())
 
-				val confirmResponse = service.loginConfirm(mapOf(
-					"loginToken" to Base64.encodeToString(cipherText, Base64.NO_WRAP),
-					"redirectUrl" to "",
-					"redirectTabUrl" to ""
-				))
+			val confirmResponse = service.loginConfirm(mapOf(
+				"loginToken" to Base64.encodeToString(cipherText, Base64.NO_WRAP),
+				"redirectUrl" to "",
+				"redirectTabUrl" to ""
+			))
 
-				confirmResponse.toObservable()
-					.flatMap<String> innerFlatMap@{ confirm ->
-						if(confirm.errorCount > 0) {
-							return@innerFlatMap Observable.error(KlasSigninFailError("failed login attempt"))
-						}
-
-						Observable.just(confirm.response.userId)
+			confirmResponse
+				.flatMap<String> innerFlatMap@{ confirm ->
+					if(confirm.errorCount > 0) {
+						return@innerFlatMap Single.error(KlasSigninFailError("failed login attempt"))
 					}
-			}
+
+					Single.just(confirm.response.userId)
+				}
+		}
 	}
 
-	fun getHome(semester: String): Observable<Home> {
-		return service.home(RequestHome(semester)).toObservable()
+	fun getHome(semester: String): Single<Home> {
+		return service.home(RequestHome(semester))
 	}
 
 	fun getSemesters(): Single<Array<Semester>> {

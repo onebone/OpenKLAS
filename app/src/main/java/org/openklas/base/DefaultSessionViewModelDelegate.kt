427950317a6145013deb2fbe2d4f381cb6465006
onebone <jyc00410@gmail.com>
2020-11-26 16:02:44 +0900

    fix(session): do not connect to network in main thread

--- app/src/main/java/org/openklas/base/DefaultSessionViewModelDelegate.kt
+++ app/src/main/java/org/openklas/base/DefaultSessionViewModelDelegate.kt
@@ -20,6 +20,7 @@ package org.openklas.base
 
 import androidx.lifecycle.MutableLiveData
 import io.reactivex.Single
+import org.openklas.net.transformer.AsyncTransformer
 import org.openklas.repository.SessionRepository
 import javax.inject.Inject
 
@@ -33,6 +34,7 @@ class DefaultSessionViewModelDelegate @Inject constructor(
 			.onErrorResumeNext { err ->
 				// TODO check if [err] is session-related error
 				sessionRepository.tryLogin()
+					.compose(AsyncTransformer())
 					.flatMap {
 						if(it) {
 							f()

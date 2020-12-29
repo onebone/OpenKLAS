154b7414faae7fa356554d2923de0030f2681d39
onebone <jyc00410@gmail.com>
2020-12-30 00:12:30 +0900

    refactor(base): remove BaseApplication

--- app/src/main/java/org/openklas/MainApplication.kt
+++ app/src/main/java/org/openklas/MainApplication.kt
@@ -18,13 +18,24 @@ package org.openklas
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import dagger.hilt.android.HiltAndroidApp
-import org.openklas.base.BaseApplication
+import org.openklas.base.Config
 import pyxis.uzuki.live.attribute.parser.annotation.AttributeParser
 
 @AttributeParser("org.openklas")
 @HiltAndroidApp
-class MainApplication : BaseApplication() {
-	override val configFilePath: String
-		get() = "config.json"
+class MainApplication: Application() {
+	override fun onCreate() {
+		super.onCreate()
+
+		Config.loadConfig(this, "config.json")
+	}
+
+	override fun attachBaseContext(base: Context) {
+		super.attachBaseContext(base)
+		MultiDex.install(this)
+	}
 }

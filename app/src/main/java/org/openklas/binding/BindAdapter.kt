b7d1ab64c96d0882661187a6ec3152f851625a7c
onebone <jyc00410@gmail.com>
2021-01-05 20:22:42 +0900

    refactor: remove unused binding code

--- app/src/main/java/org/openklas/binding/BindAdapter.kt
+++ app/src/main/java/org/openklas/binding/BindAdapter.kt
@@ -34,20 +34,6 @@ import java.util.concurrent.TimeUnit
 
 object BindAdapter {
 	@JvmStatic
-	@BindingAdapter(value = ["imageUrl", "placeholder"], requireAll = false)
-	fun bindImage(imageView: ImageView, url: String?, placeholder: Drawable?) {
-		Glide.with(imageView.context).load(url).apply(RequestOptions.placeholderOf(placeholder))
-			.into(imageView)
-	}
-
-	@JvmStatic
-	@BindingAdapter(value = ["circleImageUrl", "placeHolder"], requireAll = false)
-	fun bindCircleImage(imageView: ImageView, url: String?, placeholder: Drawable?) {
-		Glide.with(imageView.context).load(url)
-			.apply(RequestOptions.circleCropTransform().placeholder(placeholder)).into(imageView)
-	}
-
-	@JvmStatic
 	@BindingAdapter("items")
 	fun <T> bindItems(recyclerView: RecyclerView, list: Array<T>?) {
 		if(recyclerView.adapter is ListAdapter<*, *>) {

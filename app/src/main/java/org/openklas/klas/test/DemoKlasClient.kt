9a33529d5626ae0db189e76bc8984a8a03cf7f9f
onebone <jyc00410@gmail.com>
2021-02-16 12:43:18 +0900

    test(syllabus): add dummy syllabus entry

--- app/src/main/java/org/openklas/klas/test/DemoKlasClient.kt
+++ app/src/main/java/org/openklas/klas/test/DemoKlasClient.kt
@@ -21,16 +21,25 @@ package org.openklas.klas.test
 import io.reactivex.Single
 import org.openklas.klas.KlasClient
 import org.openklas.klas.model.Board
+import org.openklas.klas.model.Book
 import org.openklas.klas.model.BriefNotice
 import org.openklas.klas.model.BriefSubject
+import org.openklas.klas.model.Credits
+import org.openklas.klas.model.Expectation
 import org.openklas.klas.model.Home
+import org.openklas.klas.model.LectureMethod
+import org.openklas.klas.model.LectureType
 import org.openklas.klas.model.OnlineContentEntry
 import org.openklas.klas.model.Professor
+import org.openklas.klas.model.ScoreWeights
 import org.openklas.klas.model.Semester
 import org.openklas.klas.model.Subject
 import org.openklas.klas.model.Syllabus
 import org.openklas.klas.model.SyllabusSummary
 import org.openklas.klas.model.Timetable
+import org.openklas.klas.model.Tutor
+import org.openklas.klas.model.VL
+import org.openklas.klas.model.Week
 import org.openklas.klas.request.BoardSearchCriteria
 import java.util.Date
 import java.util.concurrent.TimeUnit
@@ -38,17 +47,13 @@ import javax.inject.Inject
 
 class DemoKlasClient @Inject constructor(): KlasClient {
 	override fun login(username: String, password: String): Single<String> {
-		return Single.fromCallable {
-			Thread.sleep(NETWORK_DELAY)
-
+		return Single.timer(NETWORK_DELAY, TimeUnit.MILLISECONDS).map {
 			"2019203999ZZ"
 		}
 	}
 
 	override fun getHome(semester: String): Single<Home> {
-		return Single.fromCallable {
-			Thread.sleep(NETWORK_DELAY)
-
+		return Single.timer(NETWORK_DELAY, TimeUnit.MILLISECONDS).map {
 			Home(
 				subjects = arrayOf(
 					Subject("U202012345678", "01", "0000-1-4567-01",
@@ -71,9 +76,7 @@ class DemoKlasClient @Inject constructor(): KlasClient {
 	}
 
 	override fun getSemesters(): Single<Array<Semester>> {
-		return Single.fromCallable {
-			Thread.sleep(NETWORK_DELAY)
-
+		return Single.timer(NETWORK_DELAY, TimeUnit.MILLISECONDS).map {
 			arrayOf(
 				Semester("2020,2", "2020년도 2학기", arrayOf(
 					BriefSubject("U202012345678", "일반상대성이론실험 - 아인슈타인", "일반상대성이론실험")
@@ -83,9 +86,7 @@ class DemoKlasClient @Inject constructor(): KlasClient {
 	}
 
 	override fun getNotices(semester: String, subjectId: String, page: Int, criteria: BoardSearchCriteria, keyword: String?): Single<Board> {
-		return Single.fromCallable {
-			Thread.sleep(NETWORK_DELAY)
-
+		return Single.timer(NETWORK_DELAY, TimeUnit.MILLISECONDS).map {
 			Board(Array(15) {
 				Board.Entry(
 					null, -1, "01", 0, 0, "01", 1, 5,
@@ -103,15 +104,13 @@ class DemoKlasClient @Inject constructor(): KlasClient {
 		criteria: BoardSearchCriteria,
 		keyword: String?
 	): Single<Board> {
-		return Single.fromCallable {
-			Thread.sleep(NETWORK_DELAY)
-
+		return Single.timer(NETWORK_DELAY, TimeUnit.MILLISECONDS).map {
 			Board(arrayOf(), Board.PageInfo(1, 0, 0, 1))
 		}
 	}
 
 	override fun getQnas(semester: String, subjectId: String, page: Int, criteria: BoardSearchCriteria, keyword: String?): Single<Board> {
-		return Single.fromCallable {
+		return Single.timer(NETWORK_DELAY, TimeUnit.MILLISECONDS).map {
 			Thread.sleep(NETWORK_DELAY)
 
 			Board(arrayOf(), Board.PageInfo(1, 0, 0, 1))
@@ -124,9 +123,7 @@ class DemoKlasClient @Inject constructor(): KlasClient {
 		keyword: String,
 		professor: String
 	): Single<Array<SyllabusSummary>> {
-		return Single.fromCallable {
-			Thread.sleep(NETWORK_DELAY)
-
+		return Single.timer(NETWORK_DELAY, TimeUnit.MILLISECONDS).map {
 			arrayOf(
 				SyllabusSummary(
 					"01", "전선", "5개기본호흡", 2021, 1, "히키가야 하치만",
@@ -145,16 +142,40 @@ class DemoKlasClient @Inject constructor(): KlasClient {
 	}
 
 	override fun getSyllabus(subjectId: String): Single<Syllabus> {
-		TODO("Too long...")
+		return Single.timer(NETWORK_DELAY, TimeUnit.MILLISECONDS).map {
+			Syllabus(
+				"5개기본호흡", "5 Fundamental Breathing", "W000", 5,
+				"0000", "01", "전선", null, 3, 4,
+				Tutor("히키가야 하치만", "박사", "", "", "whatsthat@shipduck.net"),
+				false, 0, false, 0,
+				"호흡법 중 기본 계파인 물, 번개, 바람, 화염, 바위의 호흡에 대해서 학습한다. 또한 전집중 호흡의 경지에 올라섬으로써 오니의 급습에 항시 대비할 수 있도록 한다.",
+				"전집중 호흡법을 체득하여 기본적인 체력을 증진시킴과 동시에 오니의 공격에 불시에 대항할 수 있도록 하는 것이 본 강의의 목표이다.",
+				arrayOf(Expectation("전집중 호흡", "전집중 호흡을 습득한다."), Expectation("기본 호흡", "5가지의 기본 호흡에 대한 개요를 학습한다.")),
+				Credits(1, 2, 0), null, "엑스칼리버",
+				LectureType.TEAM_TEACHING or LectureType.APPRENTICESHIP or LectureType.SMALL,
+				LectureMethod.FACE_TO_FACE_100, VL(30, 0, 0, 0, 30, 30, 10),
+				ScoreWeights(10, 30, 30, 20, 10, 0, 0),
+				arrayOf(Book("자민당, 폭발해라", "히키가야 하치만", "쇼가쿠칸", 2016), Book("역시 내 나라인 일본의 정치는 잘못되어 있다.", "히키가야 하치만", "쇼가쿠칸", 2016)),
+				"교재를 구매하지 않으면 강의를 진행할 수 없음.",
+				arrayOf(
+					Week(1, "과목 소개 (OT)", "", null),
+					Week(2, "물의 호흡 1", "", null),
+					Week(3, "물의 호흡 2", "", null),
+					Week(4, "화염의 호흡 1", "", null),
+					Week(5, "화염의 호흡 2", "", null),
+					Week(6, "바람의 호흡 1", "", null),
+					Week(7, "중간고사", "", null),
+					Week(8, "바람의 호흡 2", "", null)
+				)
+			)
+		}
 	}
 
 	override fun getOnlineContentList(
 		semester: String,
 		subjectId: String
 	): Single<Array<OnlineContentEntry>> {
-		return Single.fromCallable {
-			Thread.sleep(NETWORK_DELAY)
-
+		return Single.timer(NETWORK_DELAY, TimeUnit.MILLISECONDS).map {
 			val now = Date()
 
 			arrayOf(

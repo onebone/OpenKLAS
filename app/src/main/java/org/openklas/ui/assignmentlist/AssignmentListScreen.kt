8672e860af46117bb5a924be90c654dad5ea76f1
onebone <jyc00410@gmail.com>
2021-06-01 16:55:20 +0900

    build: upgrade dependencies
    + replace alpha with graphicsLayer to improve performance

--- app/src/main/java/org/openklas/ui/assignmentlist/AssignmentListScreen.kt
+++ app/src/main/java/org/openklas/ui/assignmentlist/AssignmentListScreen.kt
@@ -45,6 +45,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -52,7 +53,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import me.onebone.toolbar.AppbarContainer
+import me.onebone.toolbar.AppBarContainer
 import me.onebone.toolbar.CollapsingToolbar
 import me.onebone.toolbar.ScrollStrategy
 import me.onebone.toolbar.rememberCollapsingToolbarState
@@ -117,7 +118,7 @@ fun AssignmentListMainLayout(
 
 			val collapsingToolbarState = rememberCollapsingToolbarState()
 
-			AppbarContainer(
+			AppBarContainer(
 				modifier = Modifier
 					.fillMaxWidth(),
 				scrollStrategy = ScrollStrategy.ExitUntilCollapsed,
@@ -136,14 +137,9 @@ fun AssignmentListMainLayout(
 						),
 					collapsingToolbarState = collapsingToolbarState
 				) {
-					var progress by remember { mutableStateOf(1f) }
-
 					Row(
 						modifier = Modifier
 							.road(Alignment.CenterStart, Alignment.Center)
-							.progress {
-								progress = it
-							}
 					) {
 						Text(
 							text = name,
@@ -159,7 +155,9 @@ fun AssignmentListMainLayout(
 							text = stringResource(R.string.assignment_list_change_subject),
 							modifier = Modifier
 								.align(Alignment.CenterVertically)
-								.alpha(progress)
+								.graphicsLayer {
+									alpha = collapsingToolbarState.progress
+								}
 								.clickable(onClick = onClickSubjectChange)
 								.padding(4.dp),
 							color = colorResource(R.color.navigate),
